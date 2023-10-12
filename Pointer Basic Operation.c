3621945833f8f35c57a32013f4e11384c472562d
Rakib1977 <140387583+Rakib1977@users.noreply.github.com>
2023-10-12 23:38:05 +0600

    Add files via upload

---
+++ Pointer Basic Operation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int i = 5;
+    int *ip = &i;
+    printf("%d\n", *ip);
+    *ip = 7;
+    printf("%d\n", i);
+    int j = 3;
+    ip = &j;
+    printf("%d\n",*ip);
+    int *ip2;
+    ip2 = ip;
+}
