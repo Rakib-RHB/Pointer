5c2a49ce5c143d95a6a5f760e21eacf3b5f95cf5
Rakib Hasan <140387583+Rakib-RHB@users.noreply.github.com>
2023-10-18 19:53:43 +0600

    Add files via upload

---
+++ Interchanging content using function in pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *x,int *y)
+{
+    int tem;
+    tem = *x;
+    *x = *y;
+    *y = tem;
+}
+
+
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    swap(&a,&b);
+    printf("%d %d",a,b);
+
+
+}
+
